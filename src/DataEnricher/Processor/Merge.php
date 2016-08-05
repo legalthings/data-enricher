fc39a2d16bfa9defd8d5dd952d756686929c6df3
moesjarraf <moesjarraf@gmail.com>
2016-08-05 11:35:49 +0200

    Added support for merging scalars

--- src/DataEnricher/Processor/Merge.php
+++ src/DataEnricher/Processor/Merge.php
@@ -43,6 +43,10 @@ class Merge implements Processor
             if ($item instanceof Node) {
                 $item = $item->getResult();
             }
+            
+            if ($item instanceof \Traversable) {
+                $item = iterator_to_array($item);
+            }
         }
         
         return $list;
@@ -52,21 +56,33 @@ class Merge implements Processor
      * Merge properties of an object
      * 
      * @param array $list
-     * @return \stdClass|array
+     * @return \stdClass|array|string
      */
     protected function merge(array $list)
     {
+        if (empty($list)) {
+            return null;
+        }
+        
+        $scalar = false;
+        
         foreach ($list as &$item) {
             if (is_object($item)) {
                 $item = get_object_vars($item);
             }
+            
+            $scalar = is_scalar($item);
         }
-        
-        $result = call_user_func_array('array_merge', $list);
-        
-        // Is associative array
-        if (array_keys($result) !== array_keys(array_keys($result))) {
-            $result = (object)$result;
+
+        if ($scalar) {
+            $result = join('', $list);
+        } else {
+            $result = call_user_func_array('array_merge', $list);
+
+            // Is associative array
+            if (array_keys($result) !== array_keys(array_keys($result))) {
+                $result = (object)$result;
+            }
         }
         
         return $result;

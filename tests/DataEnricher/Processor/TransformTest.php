d77ba46760b4bc8d0fe5c9d88f023173c4286841
sven4ask <sven@legalthings.io>
2017-08-09 10:36:02 +0200

    Fix transform code coverage

--- tests/DataEnricher/Processor/TransformTest.php
+++ tests/DataEnricher/Processor/TransformTest.php
@@ -10,114 +10,128 @@ use LegalThings\DataEnricher\Processor;
  */
 class TransformTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * @var Processor\merge;
+     */
+    protected $processor;
+
+    public function setUp()
+    {
+        $this->processor = new Processor\Transform('<transformation>');
+    }
+    
     public function testApplyToNodeWithObjectForHash()
     {
-        $processor = new Processor\Transform('<transformation>');
-        $node = $this->getMockBuilder(Node::class)
-            ->disableOriginalConstructor()
-            ->disableProxyingToOriginalMethods()
-            ->getMock();
+        $node = $this->createMock(Node::class);
         
         $node->input = 'test';
         
         $node->expects($this->atLeastOnce())
             ->method('getInstruction')
-            ->with($processor)
+            ->with($this->processor)
             ->willReturn(['hash:sha256']);
         
         $node->expects($this->atLeastOnce())
             ->method('setResult')
             ->with(hash('sha256', 'test'));
         
-        $processor->applyToNode($node);
+        $this->processor->applyToNode($node);
+    }
+    
+    public function testApplyToNodeWithRefNode()
+    {
+        $node = $this->createMock(Node::class);
+        
+        $refNode = $this->createMock(Node::class);
+        $refNode->expects($this->atLeastOnce())
+            ->method('getResult')
+            ->willReturn('test');
+        
+        $node->input = $refNode;
+        
+        $node->expects($this->atLeastOnce())
+            ->method('getInstruction')
+            ->with($this->processor)
+            ->willReturn(['hash:sha256']);
+        
+        $node->expects($this->atLeastOnce())
+            ->method('setResult')
+            ->with(hash('sha256', 'test'));
+        
+        $this->processor->applyToNode($node);
     }
     
     public function testApplyToNodeWithObjectForStrToTimeConversion()
     {
-        $processor = new Processor\Transform('<transformation>');
-        $node = $this->getMockBuilder(Node::class)
-            ->disableOriginalConstructor()
-            ->disableProxyingToOriginalMethods()
-            ->getMock();
+        $node = $this->createMock(Node::class);
         
         $node->input = '2017-01-01T22:00:00.000Z';
         
         $node->expects($this->atLeastOnce())
             ->method('getInstruction')
-            ->with($processor)
+            ->with($this->processor)
             ->willReturn(['strtotime']);
         
         $node->expects($this->atLeastOnce())
             ->method('setResult')
             ->with(strtotime('2017-01-01T22:00:00.000Z'));
         
-        $processor->applyToNode($node);
+        $this->processor->applyToNode($node);
     }
     
     public function testApplyToNodeWithObjectForDateFormat()
     {
-        $processor = new Processor\Transform('<transformation>');
-        $node = $this->getMockBuilder(Node::class)
-            ->disableOriginalConstructor()
-            ->disableProxyingToOriginalMethods()
-            ->getMock();
+        $node = $this->createMock(Node::class);
         
         $node->input = 1483228800;
         
         $node->expects($this->atLeastOnce())
             ->method('getInstruction')
-            ->with($processor)
+            ->with($this->processor)
             ->willReturn(['date:d-m-Y']);
         
         $node->expects($this->atLeastOnce())
             ->method('setResult')
             ->with(date('d-m-Y', 1483228800));
         
-        $processor->applyToNode($node);
+        $this->processor->applyToNode($node);
     }
     
     public function testApplyToNodeWithoutInput()
     {
-        $processor = new Processor\Transform('<transformation>');
-        $node = $this->getMockBuilder(Node::class)
-            ->disableOriginalConstructor()
-            ->disableProxyingToOriginalMethods()
-            ->getMock();
+        $node = $this->createMock(Node::class);
         
         $node->input = null;
         
         $node->expects($this->atLeastOnce())
             ->method('getInstruction')
-            ->with($processor)
+            ->with($this->processor)
             ->willReturn(['hash:sha256']);
         
         $node->expects($this->never())
             ->method('setResult');
         
-        $processor->applyToNode($node);
+        $this->processor->applyToNode($node);
     }
     
     /**
-     * @expectedException PHPUnit_Framework_Error
+     * @expectedException \Exception
+     * @expectedExceptionMessage Unknown transformation 'unknown'
      */
     public function testApplyToNodeWithInvalidMethod()
     {
-        $processor = new Processor\Transform('<transformation>');
-        $node = $this->getMockBuilder(Node::class)
-            ->disableOriginalConstructor()
-            ->disableProxyingToOriginalMethods()
-            ->getMock();
+        $node = $this->createMock(Node::class);
         
         $node->input = 1483228800;
         
         $node->expects($this->atLeastOnce())
             ->method('getInstruction')
-            ->with($processor)
+            ->with($this->processor)
             ->willReturn(['unknown']);
         
         $node->expects($this->never())
             ->method('setResult');
         
-        $processor->applyToNode($node);
+        $this->processor->applyToNode($node);
     }
 }

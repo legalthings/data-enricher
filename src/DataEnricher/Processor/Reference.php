9ba3fb0a156c13f52f8953b77e81182355d67344
Moesjarraf J <moesjarraf@gmail.com>
2018-03-05 18:17:01 +0100

    Ref processor uses jmespath

--- src/DataEnricher/Processor/Reference.php
+++ src/DataEnricher/Processor/Reference.php
@@ -2,31 +2,56 @@
 
 namespace LegalThings\DataEnricher\Processor;
 
-use LegalThings\DataEnricher\Processor;
-use LegalThings\DataEnricher\Processor\Helper;
 use LegalThings\DataEnricher\Node;
+use LegalThings\DataEnricher\Processor;
+use function JmesPath\search as jmespath_search;
+use JmesPath\Utils;
 
 /**
- * Symbolic link to a property of the source object
+ * Reference JMESPath processor
+ * @see http://jmespath.org/
  */
 class Reference implements Processor
 {
-    use Processor\Implementation,
-        Helper\GetByReference
-    {
-        Helper\GetByReference::withSourceAndTarget insteadof Processor\Implementation;
-    }
+    use Processor\Implementation;
     
     /**
-     * Apply reference processing to a single node
+     * Apply processing to a single node
      * 
      * @param Node $node
      */
     public function applyToNode(Node $node)
     {
-        $ref = $node->getInstruction($this);
+        $instruction = $node->getInstruction($this);
         
-        $result = $this->getByReference($ref, $this->source, $this->target);
+        if (is_array($instruction) || is_object($instruction)) {
+            $instruction = (object)$instruction;
+        }
+        
+        $input = is_string($instruction) ? $node->getResult() : $instruction->input;
+        $query = is_string($instruction) ? $instruction : $instruction->query;
+        
+        $result = jmespath_search($query, $input);
+        static::objectivy($result);
+        
         $node->setResult($result);
+    }
+    
+    /**
+     * Cast associated arrays to objects
+     * 
+     * @return mixed
+     */
+    protected static function objectivy(&$value)
+    {
+        if (Utils::isObject($value)) {
+            $value = (object)$value;
+        }
+        
+        if (is_array($value) || is_object($value)) {
+            foreach ($value as &$item) {
+                static::objectivy($item);
+            }
+        }
     }
 }
---
+++ tests/DataEnricher/Processor/RefTest.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace LegalThings\DataEnricher\Processor;
+
+use LegalThings\DataEnricher\Node;
+use LegalThings\DataEnricher\Processor;
+
+/**
+ * @covers LegalThings\DataEnricher\Processor\JmesPath
+ */
+class JmesPathTest extends \PHPUnit_Framework_TestCase
+{
+    public function testApplyToNode()
+    {
+        // for bc
+        $processor = new Processor\Reference('<ref>');
+        
+        $node = $this->createMock(Node::class);
+        
+        $data = new \stdClass;
+        $data->foo = (object)['bar' => 'crux'];
+        
+        $node->expects($this->atLeastOnce())
+            ->method('getInstruction')
+            ->with($processor)
+            ->willReturn('foo.bar');
+        
+        $node->expects($this->atLeastOnce())
+            ->method('getResult')
+            ->willReturn($data);
+        
+        $node->expects($this->atLeastOnce())
+            ->method('setResult')
+            ->with('crux');
+        
+        $processor->applyToNode($node);
+    }
+    
+    public function testApplyToNodeAsJmesPath()
+    {
+        $processor = new Processor\Reference('<ref>');
+        
+        $node = $this->createMock(Node::class);
+        
+        $data = new \stdClass;
+        $data->foo = (object)['bar' => 'crux'];
+        
+        $node->expects($this->atLeastOnce())
+            ->method('getInstruction')
+            ->with($processor)
+            ->willReturn("foo.bar=='crux'");
+        
+        $node->expects($this->atLeastOnce())
+            ->method('getResult')
+            ->willReturn($data);
+        
+        $node->expects($this->atLeastOnce())
+            ->method('setResult')
+            ->with(true);
+        
+        $processor->applyToNode($node);
+    }
+}

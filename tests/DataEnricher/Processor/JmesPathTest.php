2790d016ec53f64afafe400d6c01e98586123f1a
Moesjarraf J <moesjarraf@gmail.com>
2018-03-26 12:53:15 +0200

    Use legalthings/jmespath.php

--- tests/DataEnricher/Processor/JmesPathTest.php
+++ tests/DataEnricher/Processor/JmesPathTest.php
@@ -20,12 +20,13 @@ class JmesPathTest extends \PHPUnit_Framework_TestCase
         $data = new \stdClass;
         $data->foo = 'red';
         $data->bar = 'Sir';
-        $data->qux = 22;
+        $data->qux = 10;
+        $data->baz = 2;
         
         $node->expects($this->atLeastOnce())
             ->method('getInstruction')
             ->with($processor)
-            ->willReturn('{foo: foo, title: bar}');
+            ->willReturn('{foo: foo, title: bar, calc: (qux * baz) + 5}');
         
         $node->expects($this->atLeastOnce())
             ->method('getResult')
@@ -33,7 +34,7 @@ class JmesPathTest extends \PHPUnit_Framework_TestCase
         
         $node->expects($this->atLeastOnce())
             ->method('setResult')
-            ->with((object)['foo' => 'red', 'title' => 'Sir']);
+            ->with((object)['foo' => 'red', 'title' => 'Sir', 'calc' => 25]);
         
         $processor->applyToNode($node);
     }
@@ -47,14 +48,15 @@ class JmesPathTest extends \PHPUnit_Framework_TestCase
         $data = new \stdClass;
         $data->foo = 'red';
         $data->bar = 'Sir';
-        $data->qux = 22;
+        $data->qux = 10;
+        $data->baz = 2;
         
         $node->expects($this->atLeastOnce())
             ->method('getInstruction')
             ->with($processor)
             ->willReturn([
                 'input' => $data,
-                'query' => '{foo: foo, title: bar}'
+                'query' => '{foo: foo, title: bar, calc: (qux * baz) + 5}'
             ]);
         
         $node->expects($this->never())
@@ -62,7 +64,7 @@ class JmesPathTest extends \PHPUnit_Framework_TestCase
         
         $node->expects($this->atLeastOnce())
             ->method('setResult')
-            ->with((object)['foo' => 'red', 'title' => 'Sir']);
+            ->with((object)['foo' => 'red', 'title' => 'Sir', 'calc' => 25]);
         
         $processor->applyToNode($node);
     }
